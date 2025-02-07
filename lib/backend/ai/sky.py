e41d26496fcac91f0e49a2fd4bbfa7112c9d07e4
zapret <elif081akdag@gmail.com>
2025-02-07 17:27:24 +0300

    Update sky.py

--- lib/backend/ai/sky.py
+++ lib/backend/ai/sky.py
@@ -1,9 +1,1 @@
-# Use a pipeline as a high-level helper
-""" from transformers import pipeline
 
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="NovaSky-AI/Sky-T1-32B-Preview")
-pipe(messages) 
-"""
